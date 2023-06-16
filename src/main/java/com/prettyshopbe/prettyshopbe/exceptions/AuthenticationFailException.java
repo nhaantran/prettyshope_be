407db6d7873c4df20f334efea338b36178dda89b
trungduc228 <76892293+trungduc228@users.noreply.github.com>
2023-06-17 01:42:30 +0700

    Delete src/main/java/com/prettyshopbe/prettyshopbe/exceptions directory

--- src/main/java/com/prettyshopbe/prettyshopbe/exceptions/AuthenticationFailException.java
+++
@@ -1,7 +0,0 @@
-package com.prettyshopbe.prettyshopbe.exceptions;
-
-public class AuthenticationFailException extends IllegalArgumentException {
-    public AuthenticationFailException(String msg) {
-        super(msg);
-    }
-}
